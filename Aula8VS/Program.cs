decb8dedc41a1214bd2cd6f230900c0f50a2645c
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-04-08 17:15:49 -0300

    @Lucas-Reluz commit 1 VS

---
+++ Aula8VS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Aula8VS.src;
+
+namespace Aula8VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Televisão primeira = new Televisão("Samsung", "4K", 4000);
+            Televisão segunda = new Televisão("LG", "2K", 2000);
+            Televisão terceira = new Televisão("AOC", "2K", 2300);
+            Televisão quarta = new Televisão("PHILCO", "FULL HD", 1800);
+
+            Console.WriteLine($"A primeira televisão é da marca {primeira.Brand}, {primeira.Resolution} de resolução e custa {primeira.Value} ");
+            Console.WriteLine($"A segunda televisão é da marca {segunda.Brand}, {segunda.Resolution} de resolução e custa {segunda.Value}");
+            Console.WriteLine($"A terceira televisão é da marca {terceira.Brand}, {terceira.Resolution} de resolução e custa {terceira.Value}");
+            Console.WriteLine($"A quarta televisão é da marca {quarta.Brand}, {quarta.Resolution} de resolução e custa {quarta.Value}");
+           
+            
+            primeira.Reso();
+            segunda.Assistir("Interestelar");
+            Console.WriteLine(terceira.Barulho());
+            quarta.Assistir("Um maluco no pedaço");
+        }
+    }
+}
---
+++ Aula8VS/src/Televisão.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Aula8VS.src
+{
+
+    public class Televisão
+    {
+        public string Brand { get; set; }
+        public string Resolution { get; set; }
+        public int    Value { get; set; }
+
+    public Televisão()
+    {
+
+    }
+
+    public Televisão(string _brand, string _resolution, int _value)
+    {
+        Brand = _brand;
+        Resolution = _resolution;
+        Value = _value;
+    }
+
+    public void Reso()
+    {
+        Console.WriteLine("Essa televisão é 4k ou 1440p");
+    }
+
+    public void Assistir(string assistir)
+    {
+        Console.WriteLine($"Nesta TV estão assistindo {assistir}");
+    }
+    public string Barulho()
+    {
+        return "plinnn.....";
+    }
+}
+}
+
